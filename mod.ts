42aceb80897c1b82ac1b1ac039b9444eb1c47597
WhyK <windchimeyk@gmail.com>
2024-12-23 22:18:33 +0900

    chore: replace deps to JSR

--- mod.ts
+++ mod.ts
@@ -1,75 +1,78 @@
-import { Status } from 'std/http/http_status.ts';
-import { contentType } from 'std/media_types/mod.ts';
+import { STATUS_CODE, StatusCode } from "@std/http/status";
+import { contentType } from "@std/media-types";
 import type { ApiResponse, RequestBody } from "./model.ts";
 
 const responseInit = (
   contentType: string,
-  status: Status = Status.OK,
+  status: StatusCode = STATUS_CODE.OK,
 ): ResponseInit => ({
   headers: { "content-type": contentType },
   status,
 });
 
-const responseText = (text: string, status: Status = Status.OK) => new Response(text, responseInit(contentType("txt"), status))
+const responseText = (text: string, status: StatusCode = STATUS_CODE.OK) =>
+  new Response(text, responseInit(contentType("txt"), status));
 
-const responseJson = (res: ApiResponse, status: Status) =>
+const responseJson = (res: ApiResponse, status: StatusCode) =>
   new Response(JSON.stringify(res), responseInit(contentType("json"), status));
 
 const handler: Deno.ServeHandler = async (req) => {
   const { pathname } = new URL(req.url);
 
   if (pathname === "/") {
-    return responseText("このAPIはAPI KEYがないと使えません。API KEYは運営者から受け取ってください")
+    return responseText(
+      "このAPIはAPI KEYがないと使えません。API KEYは運営者から受け取ってください",
+    );
   }
 
   if (
     pathname === "/api" &&
     req.headers.get("x-api-key") !== Deno.env.get("X_API_KEY")
   ) {
-    return responseText("APIキーがありません", Status.Unauthorized)
+    return responseText("APIキーがありません", STATUS_CODE.Unauthorized);
   }
 
   const bodyReader = await req.body?.getReader().read();
   const decoder = new TextDecoder();
-  const body: RequestBody = JSON.parse(decoder.decode(bodyReader?.value))
+  const body: RequestBody = JSON.parse(decoder.decode(bodyReader?.value));
 
   const isApiUserId = (userId: string) =>
     pathname === "/api" && body.user_id === userId;
 
   if (isApiUserId("200")) {
     return responseJson({
-      code: `API-${Status.OK}`,
+      code: `API-${STATUS_CODE.OK}`,
       message: "OK",
       contents: {
         text: "成功してますよ",
       },
-    }, Status.OK);
+    }, STATUS_CODE.OK);
   }
 
   if (isApiUserId("400")) {
     return responseJson({
-      code: `API-${Status.BadRequest}`,
+      code: `API-${STATUS_CODE.BadRequest}`,
       message: "Bad Request",
       error: {
         type: "Parameter Error",
         code: "API00BR",
       },
-    }, Status.BadRequest);
+    }, STATUS_CODE.BadRequest);
   }
 
   if (isApiUserId("500")) {
     return responseJson({
-      code: `API-${Status.InternalServerError}`,
+      code: `API-${STATUS_CODE.InternalServerError}`,
       message: "Internal Server Error",
       error: {
         type: "Server Error",
         code: "API01ISE",
       },
-    }, Status.InternalServerError);
+    }, STATUS_CODE.InternalServerError);
   }
 
-  return responseText("お探しのコンテンツはありません", Status.NotFound)
+  return responseText("お探しのコンテンツはありません", STATUS_CODE.NotFound);
 };
 
 const PORT = 8080;
-Deno.serve({ port: PORT }, handler)
+Deno.serve({ port: PORT }, handler);
